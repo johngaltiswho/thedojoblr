216bd13020083da3acdc7bda235620af81423af2
Uma Maheshwar Reddy <home@Umas-MacBook-Air.local>
2021-06-13 20:27:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+const morgan = require('morgan');
+const winston = require('winston');
+const dontenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+var app = express()
+
+app.use(cors());
+
+// //sengrid parameters
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// var forceSsl = function (req, res, next) {
+//     if ((req.headers['x-forwarded-proto'] !== 'https') && (app.get('env') === "'production'")) {
+//         console.log('Succesfully Redirected to HTTPS')
+//         return res.redirect(301, ['https://', req.get('Host'), req.url].join(''));
+//     } else {
+//       console.log( 'No need to re-direct to HTTPS' );
+//       next();
+//     }
+//  };
+//
+// console.log(app.get('env'))
+// app.use(forceSsl);
+
+//code snippet for www. domain to non www. domain name
+// function wwwRedirect(req, res, next) {
+//     if ((req.headers.host.slice(0, 4) != 'www.') && (app.get('env') === "'production'")) {
+//       console.log(req.headers.host);
+//       console.log(req.protocol);
+//       console.log(req.originalUrl);
+//       return res.redirect(301, req.protocol + '://www.' + req.headers.host + req.originalUrl);
+//       console.log("Redirect successful")
+//     }
+//     next();
+// };
+//
+// app.set('trust proxy', true);
+// app.use(wwwRedirect);
+
+const url = process.env.MONGODB_URL_ATLAS;
+const connect = mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+connect.then((db) => {
+  console.log('Connected correctly to server');
+}, (err) => {winston.error(err); });
+
+// app.use(morgan('combined', { stream: winston.stream }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+console.log(app.get('env'))
+
+if (app.get('env') === 'production') {
+  app.use('/', express.static(path.join(__dirname, '/client/build')));
+}
+
+if (app.get('env') === 'production') {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  });
+}
+
+app.use((req, res) => {
+  res.status(200).send(req.originalUrl);
+});
+
+// * * * * *
+app.listen(process.env.DEVELOPMENTPORT, () => {
+ console.log("Server listening on port " + process.env.DEVELOPMENTPORT);
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // add this line to include winston logging
+  console.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+  // render the error page
+  res.status(err.status || 500);
+  next(err);
+});
+
+module.exporst = app
---
+++ models/enquiry.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+var validateNumber = function(phone) {
+  var re = /^[6-9]\d{9}$/;
+  return re.test(phone);
+}
+
+const enquirySchema = new Schema({
+    name: {
+      type: String,
+      required: true,
+      required: 'Please enter your name',
+      trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        required: 'Please enter your email',
+        trim: true,
+        lowercase:true,
+        validate: [validateEmail, 'Please fill a valid email address!' ]
+    },
+    phone: {
+        type: String,
+        required: true,
+        required: 'Please enter your phone number',
+        validate: [validateNumber, 'Please fill a valid phone number!' ]
+    },
+    subject: {
+      type: String,
+      required: true,
+      required: 'Please enter a subject'
+    },
+    message: {
+      type: String,
+      required: true,
+      required: 'Please enter a message'
+    }
+},{
+    timestamps: true
+});
+
+var enquiry = mongoose.model('enquiry', contactusSchema);
+
+module.exports = enquiry;
